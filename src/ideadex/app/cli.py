636851b96a6569d301d0e58dd9425bb4773e1bf2
Valdack <cp9g2hsk8v@privaterelay.appleid.com>
2026-01-18 21:53:25 +0100

    implementation de la fonction load_cards()

--- src/ideadex/app/cli.py
+++ src/ideadex/app/cli.py
@@ -1,5 +1,9 @@
+from ideadex.data.repository import load_cards
+
+
 def main():
     print("Hello from CLI")
+    print(load_cards())
 
 
 if __name__ == "__main__":
--- src/ideadex/data/repository.py
+++ src/ideadex/data/repository.py
@@ -1,11 +1,27 @@
-from src.ideadex.domain import Card, Relation
+import json
+from ideadex.domain import Card, Relation
+
 
 # TODO : V <- compléter ces fonctions pour load des cartes et sauvegarder des relations
 # Il faudra utiliser json dans un premier temps puis on passera en mode DB
 # Ton but est de faire une interface pour rendre le code agnostique de l'implémentation.
 
 
-def load_cards() -> list[Card]: ...
+def load_cards() -> list[Card]:
+    with open("cards.json", "r") as file:
+        data = json.load(file)
+    liste = []
+    for dic in data:
+        liste.append(
+            Card(
+                id=dic["id"],
+                title=dic["title"],
+                description=dic["description"],
+                themes=dic["themes"],
+            )
+        )
+    return liste
 
 
-def save_relation(relation: Relation) -> None: ...
+def save_relation(relation: Relation) -> None:
+    ...
